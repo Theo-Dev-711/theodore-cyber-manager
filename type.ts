b99e5788342b79994e49fc4d3457bfec3ed58bd3
Theo Dev <juniordev711@gmail.com>
2025-10-13 02:36:47 +0100

    up4

--- type.ts
+++ type.ts
@@ -1,18 +1,25 @@
+// type.ts
+import { Transaction as PrismaTransaction, Category } from "@prisma/client";
 
-import { Category, Transaction as PrismaTransaction } from "@prisma/client";
-
+/**
+ * Représente une transaction avec les infos de la catégorie et imageUrl
+ */
 export interface TransactionWithCategory extends PrismaTransaction {
+  imageUrl: string | null; // image optionnelle
   category?: Category | null; // relation incluse
-  categoryName?: string; // champ dérivé
+  categoryName: string; // champ dérivé pour affichage
 }
 
+/**
+ * Formulaire de création ou mise à jour de transaction
+ */
 export interface FormDataType {
   id?: string;
   name: string;
   amount: number;
-  description: string;
-  type: string;
+  description?: string;
+  type: string // Enum TypeTransaction
   imageUrl?: string | null;
-  categoryId?: string;
+  categoryId: string;
   categoryName?: string;
 }

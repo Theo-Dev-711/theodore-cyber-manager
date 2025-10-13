e55b166cec66700f0c2b96422afa4f21dd24f288
Theo Dev <juniordev711@gmail.com>
2025-10-13 15:50:06 +0100

    up14

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div className=' text-black text-center text-6xl bg-red-60'>
-      Acceuil
+     Bienvenue ici nous vous partagerons l'actulite de l'application et du management
+     nous vous reviendrons dans un few moment
     </div>
   )
 }
